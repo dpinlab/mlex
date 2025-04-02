bc431d4960522bb52e4fdb57087b1cd5c54152b2
GIL32610 <gil.00000032610@unicap.br>
2025-04-02 17:36:38 -0300

    #1 create LSTMModule

--- mlex/torch_modules/models.py
+++ mlex/torch_modules/models.py
@@ -22,4 +22,12 @@ class RNNModule(BaseMLEXModule):
         nn.RNN(input_size=self.input_size, hidden_size=self.hidden_size, num_layers=self.num_layers, batch_first=True),
         nn.Linear(in_features=hidden_size, out_features=num_classes)
         )
-        
+
+
+class LSTMModule(BaseMLEXModule):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super().__init__(self, RNNModule, input_size, hidden_size, num_layers, num_classes)
+        self.model = nn.Sequential(
+        nn.LSTM(input_size=self.input_size, hidden_size=self.hidden_size, num_layers=self.num_layers, batch_first=True),
+        nn.Linear(in_features=hidden_size, out_features=num_classes)
+        )
