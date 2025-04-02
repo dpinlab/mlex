2a28f64691e229e1b7d3ae8c66978b5ed21a9e9b
GIL32610 <gil.00000032610@unicap.br>
2025-04-02 17:24:59 -0300

    #1 create BaseMLEXModule abc

--- mlex/torch_modules/models.py
+++ mlex/torch_modules/models.py
@@ -1,15 +1,25 @@
 import torch.nn as nn
 import torch.nn.functional as F
+from abc import ABC
 
-class RNNModule(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, num_classes):
-        super(RNNModule,self).__init__()
+class BaseMLEXModule(nn.Module, ABC):
+    def __init__(self, module,input_size, hidden_size, num_layers, num_classes):
+        super(module,self).__init__()
+        self.input_size = input_size
         self.hidden_size = hidden_size
         self.num_layers = num_layers
-        self.rnn = nn.RNN(input_size=input_size,hidden_size=hidden_size,num_layers=num_layers, batch_first=True)
-        self.output_layer = nn.Linear(in_features=hidden_size, out_features=num_classes)
-        
-    def forward(self, x):
-        ht = self.rnn(x)
+        self.num_classes = num_classes
+
+    def forward(self,x):
+        ht = self.model(x)
         z = self.output_layer
         return F.sigmoid(z)
+
+class RNNModule(BaseMLEXModule):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super().__init__(self, RNNModule, input_size, hidden_size, num_layers, num_classes)
+        self.model = nn.Sequential(
+        nn.RNN(input_size=self.input_size, hidden_size=self.hidden_size, num_layers=self.num_layers, batch_first=True),
+        nn.Linear(in_features=hidden_size, out_features=num_classes)
+        )
+        
