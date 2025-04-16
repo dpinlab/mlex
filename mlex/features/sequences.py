098d0876943f0abe992d2c15796f8609ded74d6d
isadoravrx <isavitoriaxavier@gmail.com>
2025-04-15 23:33:38 -0300

    Adding part of the implementation of the sequence and dataloader part

--- mlex/features/sequences.py
+++ mlex/features/sequences.py
@@ -1,41 +1,31 @@
-
+import torch
+from torch.utils.data import Dataset, DataLoader
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
-from functools import reduce
-import itertools as ite
-import tensorflow as tf
 
-class SequenceTransfomer(BaseEstimator, TransformerMixin):
-    
-    def __init__(self, 
-                 sequence_length = 10,
-                 batch_size = 32
-                 ) -> None:
-        super().__init__()
+class SequenceDataset(Dataset):
+    def __init__(self, X, y, sequence_length):
+        self.X = torch.tensor(X, dtype=torch.float32)
+        self.y = torch.tensor(y, dtype=torch.float32)
+        self.sequence_length = sequence_length
+
+    def __len__(self):
+        return len(self.X) - self.sequence_length + 1
+
+    def __getitem__(self, idx):
+        X_seq = self.X[idx:idx + self.sequence_length]
+        y_seq = self.y[idx + self.sequence_length - 1]  
+        return X_seq, y_seq
+
+class SequenceTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, sequence_length=10, batch_size=32):
         self.sequence_length = sequence_length
         self.batch_size = batch_size
-       
-        
+
     def fit(self, X, y=None):
         return self
-    
-    def transform(self, X, y, **fit_params):
-        data = tf.keras.utils.timeseries_dataset_from_array(
-            # X[:-self.sequence_length],
-            # y[self.sequence_length:],
-            X,
-            y,
-            sequence_length=self.sequence_length,
-            sequence_stride=1,
-            sampling_rate=1,
-            batch_size=self.batch_size,
-            shuffle=False, #mistura de dados
-            seed=None,
-            start_index=None,
-            end_index=None,
-        )
-        return data
 
-
-
-
+    def transform(self, X, y):
+        dataset = SequenceDataset(X, y, self.sequence_length)
+        dataloader = DataLoader(dataset, batch_size=self.batch_size, shuffle=False)
+        return dataloader
--- mlex/utils/split.py
+++ mlex/utils/split.py
@@ -1,7 +1,7 @@
 import abc
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from mlex.utils.analysis import LacciAnalysis
+from mlex.utils.preprocessing import PreProcessing
 
 
 class BaseSplitStrategy(abc.ABC):
@@ -25,37 +25,6 @@ class PastFutureSplit(BaseSplitStrategy):
         y_test = self.y[mid:-1]
         return X_train, X_test, y_train, y_test
     
-class AccountStratifiedSplit(BaseSplitStrategy):
-
-    def train_test_split(self, typology='I-d', proportion=.3):
-
-        id_counts = self.df[self.df[typology] == 1].groupby('CONTA_TITULAR').size()
-        total_counts = self.df.groupby('CONTA_TITULAR').size()
-        id_ratio = (id_counts / total_counts).fillna(0)
-
-        accounts_df = pd.DataFrame({"CONTA_TITULAR": total_counts.index, "id_ratio": id_ratio})
-        accounts_df["cluster"] = pd.cut(accounts_df["id_ratio"], bins=[-0.01, 0, 0.25, 0.5, 1.0], labels=[0, 1, 2, 3])
-
-        train_accounts, test_accounts = train_test_split(
-            accounts_df["CONTA_TITULAR"], test_size=proportion, stratify=accounts_df["cluster"]
-            )
-
-        train_df = self.df[self.df['CONTA_TITULAR'].isin(train_accounts)]
-        test_df = self.df[self.df['CONTA_TITULAR'].isin(test_accounts)]
-
-        common_values = set(train_df['CNAB']).intersection(set(test_df['CNAB']))
-
-        train_df = train_df[train_df['CNAB'].isin(common_values)]
-        test_df = test_df[test_df['CNAB'].isin(common_values)]
-
-        lacci_analysis_train = LacciAnalysis(train_df)
-        lacci_analysis_test = LacciAnalysis(test_df)
-
-        X_train, y_train, feature_names_train = lacci_analysis_train.get_X_y()
-        X_test, y_test, feature_names_test = lacci_analysis_test.get_X_y()
-
-        return X_train, X_test, train_df['CONTA_TITULAR'], y_train, y_test, feature_names_train
-    
 class AccountStratifiedSplit:
     def train_test_split(self, typology='I-d', proportion=.3):
 
@@ -78,8 +47,8 @@ class AccountStratifiedSplit:
         train_df = train_df[train_df['CNAB'].isin(common_values)]
         test_df = test_df[test_df['CNAB'].isin(common_values)]
 
-        lacci_analysis_train = LacciAnalysis(train_df)
-        lacci_analysis_test = LacciAnalysis(test_df)
+        lacci_analysis_train = PreProcessing(train_df)
+        lacci_analysis_test = PreProcessing(test_df)
 
         X_train, y_train, feature_names_train = lacci_analysis_train.get_X_y()
         X_test, y_test, feature_names_test = lacci_analysis_test.get_X_y()
@@ -109,8 +78,8 @@ class CpfStratifiedSplit:
         train_df = train_df[train_df['CNAB'].isin(common_values)]
         test_df = test_df[test_df['CNAB'].isin(common_values)]
 
-        lacci_analysis_train = LacciAnalysis(train_df)
-        lacci_analysis_test = LacciAnalysis(test_df)
+        lacci_analysis_train = PreProcessing(train_df)
+        lacci_analysis_test = PreProcessing(test_df)
 
         X_train, y_train, feature_names_train = lacci_analysis_train.get_X_y()
         X_test, y_test, feature_names_test = lacci_analysis_test.get_X_y()
