ea4da78ce8b5202e384ce2522c134ced5c645ba9
isadoravrx <isavitoriaxavier@gmail.com>
2025-10-22 23:25:28 -0300

    Updated : updating random_seed optional and CI experiment in benchmark

--- experiments/benchmarks/eye_detection/GRU/generate_plots_with_ci.py
+++ experiments/benchmarks/eye_detection/GRU/generate_plots_with_ci.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../..'))
+module_path = os.path.abspath(os.path.join('../../../..'))
 if module_path not in sys.path:
     sys.path.append(module_path)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
@@ -22,6 +22,7 @@ lengths   = ['10', '20', '30', '40', '50']
 thresholds_list = ['f1max']
 iterations = 10
 iteration = 10
+clusters = ['kmeans', 'gmm', 'agglomerative'] 
 num_layers = 1
 hidden_size = 10
 
@@ -31,19 +32,38 @@ plotter = EvaluationPlotter(f"evaluation.parquet")
 
 # Generate the strings
 formatted_ids = [
-    f"{model}_Layers-{num_layers}_HiddenSize-{hidden_size}_SequenceLength-{length}_{sequence}_{threshold}_Iteration-{i+1}"
-    for model, length, sequence, threshold, i in product(models, lengths, sequences, thresholds_list, range(iterations))
+    f"{model}_Layers-{num_layers}_HiddenSize-{hidden_size}_SequenceLength-{length}_{sequence}_{threshold}_Iteration-{i+1}_{cluster}"
+    for model, length, sequence, threshold, i, cluster in product(
+        models, lengths, sequences, thresholds_list, range(iterations), clusters
+    )
 ]
 
-list_model_ids = [formatted_ids[i:i+iterations] for i in range(0, len(formatted_ids), iterations)]
+def get_label(group_id: str):
+    parts = group_id.split('_')
+    cluster = parts[-1].capitalize()  
+    context = parts[4].capitalize()   
+    return f"{cluster} - {context} "
 
-list_model_ids = [list_model_ids[i:i+len(sequences)] for i in range(0, len(list_model_ids), len(sequences))]
 
-for model_ids in list_model_ids:
-    string_plot = model_ids[0][0].rsplit("_", 3)[0]
+
+for sequence_length in lengths:
+    model_ids_for_length = [
+        [mid for mid in formatted_ids 
+         if f"SequenceLength-{sequence_length}_" in mid and f"_{cluster}" in mid and f"_{seq}_" in mid]
+        for cluster, seq in product(clusters, sequences)
+    ]
+
+    # remove grupos vazios
+    model_ids_for_length = [g for g in model_ids_for_length if g]
+
+    if not model_ids_for_length:  
+        continue
+
+    string_plot = f"seq-{sequence_length}"
 
     fig, ax = plt.subplots()
-    plotter.plot_roc_curve_with_ci(model_ids, ax=ax)
+    labels = [get_label(group[0]) for group in model_ids_for_length]
+    plotter.plot_roc_curve_with_ci(model_ids_for_length, ax=ax, labels=labels,shade=False)
     plt.savefig(join(save_path, f"{string_plot}_eye_detection-roc_curve_with_ci.pdf"), format="pdf", dpi=300)
     plt.show()
     plt.close(fig)
@@ -56,7 +76,7 @@ for model_ids in list_model_ids:
 
     fig, ax = plt.subplots()
     plotter.plot_metric_history_with_ci(
-        model_groups=model_ids,
+        model_groups=model_ids_for_length,
         metric="f1",
         ax=ax
     )
