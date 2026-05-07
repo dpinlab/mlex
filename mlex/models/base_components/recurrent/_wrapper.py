bddf301c23a96ff81ebc7d51df7ca85a579a61f4
William Teles <teles967@gmail.com>
2026-05-07 09:14:47 -0300

    (PR#20) 19/refactor-wrapper-rnns #19

--- mlex/models/base_components/recurrent/_wrapper.py
+++ mlex/models/base_components/recurrent/_wrapper.py
@@ -109,43 +109,11 @@ class RecurrentModel(nn.Module, BaseEstimator, ClassifierMixin):
         test_data = kwargs.pop('test_data', None)
         self._update_params(kwargs)
 
-        if self.filter_dict is not None:
-            X, y = self._apply_filter(X, y)
-
-        X, y = self._apply_context_aware_transform(X, y)
+        X, y = self._prepare_input(X, y)
         self._set_categories(X)
         self.train_data = (X, y)
         self._prepare_validation_split(X, y)
-
-        if self.model_params.input_size is None:
-            preprocessor = PreProcessingTransformer(
-                target_column=[self.target_column],
-                **self.preprocessor_params.model_dump(),
-                categories=self.categories,
-                handle_unknown='ignore',
-            )
-            preprocessor.fit(self.train_data[0])
-            feature_names = preprocessor.get_feature_names_out()
-            X_val_transformed, y_val_transformed = preprocessor.transform(
-                self.val_data[0], self.val_data[1]
-            )
-
-            observers = list(self.model_params.epoch_observers or [])
-            if test_data is not None:
-                X_test_transformed, y_test_transformed = preprocessor.transform(
-                    test_data[0], test_data[1]
-                )
-                observers.append(
-                    AUCROCObserver(name='test', data=(X_test_transformed, y_test_transformed))
-                )
-
-            self.model_params = self.model_params.model_copy(update={
-                'feature_names': feature_names,
-                'input_size': feature_names.shape[0] - 1,
-                'validation_data': (X_val_transformed, y_val_transformed),
-                'epoch_observers': observers,
-            })
-            self.model = self._build_model()
+        self._autoconfigure(test_data)
 
         start = time.perf_counter()
         self.model.fit(self.train_data[0], self.train_data[1])
@@ -156,36 +124,60 @@ class RecurrentModel(nn.Module, BaseEstimator, ClassifierMixin):
         self.final_model.fitted_ = True
         return self
 
+    def _autoconfigure(self, test_data=None):
+        if self.model_params.input_size is not None:
+            return
+
+        preprocessor = PreProcessingTransformer(
+            target_column=[self.target_column],
+            **self.preprocessor_params.model_dump(),
+            categories=self.categories,
+            handle_unknown='ignore',
+        )
+        preprocessor.fit(self.train_data[0])
+        feature_names = preprocessor.get_feature_names_out()
+        X_val_transformed, y_val_transformed = preprocessor.transform(
+            self.val_data[0], self.val_data[1]
+        )
+
+        observers = list(self.model_params.epoch_observers or [])
+        if test_data is not None:
+            X_test_transformed, y_test_transformed = preprocessor.transform(
+                test_data[0], test_data[1]
+            )
+            observers.append(
+                AUCROCObserver(name='test', data=(X_test_transformed, y_test_transformed))
+            )
+
+        self.model_params = self.model_params.model_copy(update={
+            'feature_names': feature_names,
+            'input_size': feature_names.shape[0] - 1,
+            'validation_data': (X_val_transformed, y_val_transformed),
+            'epoch_observers': observers,
+        })
+        self.model = self._build_model()
+
     def predict_proba(self, X):
         self._validate_fitted()
-
-        if self.filter_dict is not None:
-            X_filtered = self._apply_filter(X)
-        else:
-            X_filtered = X.copy()
-
-        X_sorted = self._sort_by_context(X_filtered)
-
-        seq_pred = self.model.predict(X_sorted)
-        seq_end_indices = self.final_model.predict_end_indices
-
-        if len(seq_pred) == 0:
+        X_sorted, _ = self._prepare_input(X)
+        y_pred_sorted, seq_end_indices = self._predict_sorted(X_sorted)
+        if len(seq_end_indices) == 0:
             return np.zeros(len(X))
-
         if self.model_params.collect_activations:
             if '__orig_index' in X_sorted.columns:
                 self.activation_indices_ = X_sorted['__orig_index'].values[seq_end_indices]
             else:
                 self.activation_indices_ = X_sorted.index[seq_end_indices]
-
-        y_pred_sorted = self._assemble_sorted_predictions(X_sorted, seq_end_indices, seq_pred)
-
         return self._map_to_original_order(X, X_sorted, y_pred_sorted)
 
     def predict(self, X):
         self._validate_fitted()
         y_pred = self.predict_proba(X)
         return (y_pred >= self.threshold).astype(int)
+
+    def _prepare_input(self, X, y=None):
+        X, y = self._apply_filter(X, y)
+        return self._apply_context_aware_transform(X, y)
 
     def _apply_context_aware_transform(self, X, y=None):
         if self.timestamp_column is not None:
@@ -216,7 +208,7 @@ class RecurrentModel(nn.Module, BaseEstimator, ClassifierMixin):
             y_filtered = y[mask].copy()
             return X_filtered, y_filtered
 
-        return X_filtered
+        return X_filtered, None
 
     def _set_categories(self, X):
         categorical_features = self.preprocessor_params.categorical_features
@@ -246,34 +238,6 @@ class RecurrentModel(nn.Module, BaseEstimator, ClassifierMixin):
         if not self.fitted_:
             raise ValueError("Model is not fitted")
 
-    def _sort_by_context(self, X):
-        X_sorted, _ = self._apply_context_aware_transform(X.copy(), None)
-        return X_sorted
-
-    def _build_sequence_windows(self, X_sorted, X_transformed):
-        seq_len = self.model_params.seq_length
-        X_transformed = np.asarray(X_transformed)
-
-        if self.context_column is None:
-            groups = [list(range(len(X_sorted)))]
-        else:
-            groups = [list(idxs) for idxs in X_sorted.groupby(self.context_column).groups.values()]
-
-        windows = []
-        end_positions = []
-
-        for group_idxs in groups:
-            if len(group_idxs) < seq_len:
-                continue
-            for i in range(seq_len - 1, len(group_idxs)):
-                start_pos = group_idxs[i - seq_len + 1]
-                end_pos = group_idxs[i]
-                window = X_transformed[start_pos:end_pos + 1, :-1]
-                windows.append(window)
-                end_positions.append(end_pos)
-
-        return windows, end_positions
-
     def _assemble_sorted_predictions(self, X_sorted, end_positions, seq_scores):
         y_pred = np.zeros(len(X_sorted), dtype=float)
         y_pred[np.array(end_positions, dtype=int)] = seq_scores
@@ -290,10 +254,14 @@ class RecurrentModel(nn.Module, BaseEstimator, ClassifierMixin):
 
         return y_pred
 
-    def _set_threshold(self):
-        pred = self.model.predict(self.val_data[0])
+    def _predict_sorted(self, X_sorted):
+        seq_pred = self.model.predict(X_sorted)
         seq_end_indices = self.final_model.predict_end_indices
-        y_pred = self._assemble_sorted_predictions(self.val_data[0], seq_end_indices, pred)
+        y_pred_sorted = self._assemble_sorted_predictions(X_sorted, seq_end_indices, seq_pred)
+        return y_pred_sorted, seq_end_indices
+
+    def _set_threshold(self):
+        y_pred, _ = self._predict_sorted(self.val_data[0])
         threshold_strat = F1MaxThresholdStrategy()
         self.threshold = threshold_strat.compute_threshold(self.val_data[1], y_pred)
 
@@ -326,12 +294,6 @@ class RecurrentModel(nn.Module, BaseEstimator, ClassifierMixin):
             self.threshold = parameters.pop('threshold')
         self._update_params(parameters)
         return self
-
-    def create_test_loader(self, X, y):
-        self._validate_fitted()
-        X, y = self._apply_context_aware_transform(X, y)
-        X = self.model.named_steps['preprocessor'].transform(X)
-        return self.final_model._create_dataloader(X, y, shuffle_dataloader=False)
 
     def get_y_true_sequences(self, X, y):
         self._validate_fitted()
